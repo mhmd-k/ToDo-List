f0f8f47942650226b4f627aaf57ce5d422d28158
Mohammed Alkayyali <mhmdk425@gmail.com>
2022-08-30 06:42:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-let addTaskBtn = document.querySelector("#new-task-submit");
-let newTask = document.querySelector(".enter input");
+const addTaskBtn = document.querySelector("#new-task-submit");
+const newTask = document.querySelector(".enter input");
+const clearBtn = document.getElementById("clear");
+const messageOne = document.querySelector("h1 + span");
+const messageTow = document.querySelector("h2 + span");
 let storageArray = [];
 
 // creat task function
 function creatTask(taskValue) {
+  clearBtn.classList.add("visible");
   let task = document.createElement("div");
   task.classList.add("task");
   document.querySelector(".content").append(task);
@@ -34,7 +38,19 @@ function creatTask(taskValue) {
   // delete task
   deleteBtn.onclick = () => {
     deleteTask(deleteBtn);
+    if (!document.querySelector(".tasks .content").hasChildNodes()) {
+      clearBtn.classList.remove("visible");
+    }
   };
+}
+
+// message function
+function message(ele, eleClass, text) {
+  ele.innerHTML = text;
+  ele.classList.add(eleClass);
+  setTimeout(() => {
+    ele.classList.remove(eleClass);
+  }, 3000);
 }
 
 // delete task function
@@ -53,6 +69,16 @@ function deleteTask(e) {
 function editeTask(e) {
   if (e.innerHTML == "EDIT") {
     e.innerHTML = "DONE";
+    e.parentElement.firstChild.focus();
+    window.onbeforeunload = function () {
+      if (e.parentElement.firstChild.value === "") {
+        return "";
+      }
+    };
+    document.querySelectorAll("#edit").forEach((btn) => {
+      btn.style.setProperty("pointer-events", "none");
+    });
+    e.style.setProperty("pointer-events", "visible");
     e.parentElement.firstChild.removeAttribute("readonly");
     let t = JSON.parse(localStorage.getItem("tasks"));
     t.forEach((ele) => {
@@ -63,12 +89,11 @@ function editeTask(e) {
     localStorage.setItem("tasks", JSON.stringify(t));
   } else {
     if (e.parentElement.firstChild.value === "") {
-      console.log("enter any letter");
-      document.querySelector("h2 + p").classList.add("show");
-      setTimeout(() => {
-        document.querySelector("h2 + p").classList.remove("show");
-      }, 4000);
+      message(messageTow, "empty", "Task can't be empty");
     } else {
+      document.querySelectorAll("#edit").forEach((btn) => {
+        btn.style.setProperty("pointer-events", "visible");
+      });
       e.innerHTML = "EDIT";
       e.parentElement.firstChild.setAttribute("readonly", true);
       let t = JSON.parse(localStorage.getItem("tasks"));
@@ -82,33 +107,59 @@ function editeTask(e) {
   }
 }
 
+// check if the task is repeated function
+function checkIfRepeated(contentDiv) {
+  if (contentDiv.children.length > 0) {
+    let tasksArray = Array.from(contentDiv.children);
+    tasksArray = tasksArray.map((e) => {
+      return e.firstChild.value;
+    });
+    let similar = false;
+    tasksArray.forEach((e) => {
+      if (e === newTask.value) {
+        similar = true;
+      }
+    });
+    if (similar === false) {
+      creatTask(newTask.value);
+      message(messageOne, "added", "Task has been added");
+    }
+    message(messageOne, "empty", "There is already a task with the same name");
+  } else {
+    creatTask(newTask.value);
+    message(messageOne, "added", "Task has been added");
+  }
+}
+
+// add new task when pressing the button
 addTaskBtn.addEventListener("click", () => {
   if (newTask.value == "" || newTask.value == null) {
-    document.querySelector("h1 + p").classList.add("show");
-    setTimeout(() => {
-      document.querySelector("h1 + p").classList.remove("show");
-    }, 4000);
+    message(messageOne, "empty", "Task can't be an empty string");
   } else {
-    //   check if the task is repeated
-    if (document.querySelector(".content").children.length > 0) {
-      let tasksArray = Array.from(document.querySelector(".content").children);
-      tasksArray = tasksArray.map((e) => {
-        return e.firstChild.value;
-      });
-      let similar = false;
-      tasksArray.forEach((e) => {
-        if (e === newTask.value) {
-          similar = true;
-        }
-      });
-      if (similar === false) {
-        creatTask(newTask.value);
-      }
-    } else {
-      creatTask(newTask.value);
-    }
+    checkIfRepeated(document.querySelector(".content"));
     newTask.value = "";
   }
+});
+
+// add new task when pressing ENTER
+newTask.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    if (newTask.value == "" || newTask.value == null) {
+      message(messageOne, "empty", "Task can't be an empty string");
+    } else {
+      checkIfRepeated(document.querySelector(".content"));
+      newTask.value = "";
+      message(messageOne, "added", "Task has been added");
+    }
+  }
+});
+
+// delete all tasks
+clearBtn.addEventListener("click", () => {
+  document.querySelector(".content").innerHTML = "";
+  localStorage.removeItem("tasks");
+  clearBtn.classList.remove("visible");
+  message(messageOne, "added", "All tasks deleted");
 });
 
 // get items from local storage
@@ -116,5 +167,8 @@ if (localStorage.length !== 0) {
   let t = JSON.parse(localStorage.getItem("tasks"));
   for (let i = 0; i < t.length; i++) {
     if (!t[i].completed && t[i].name !== "") creatTask(t[i].name);
+  }
+  if (document.querySelector(".tasks .content").hasChildNodes()) {
+    clearBtn.classList.add("visible");
   }
 }
